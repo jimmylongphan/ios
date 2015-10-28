498415b4a3417e77f4b4b9f372b4ac4d6a10b903
Jimmy Phan <jimmyphan@yahoo-inc.com>
2015-10-28 00:07:09 -0700

    interactive

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func submit(sender: AnyObject) {
+    
+        print("Button tapped")
+    
+        label.text = textField.text
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello Rob!")
     }
 
     override func didReceiveMemoryWarning() {
